e441c04004dfa87acf11c7bfdfda1cdce5067e1c
Lemespien <Nilhogg@gmail.com>
2020-05-24 15:44:31 +0200

    Added check if idle to creations module. Made a rough rebirth loop.

--- creations.py
+++ creations.py
@@ -19,24 +19,23 @@ class Creations(Tabs):
 
     SHADOW_CLONES = IMAGE_DICTIONARY["shadow_clones"]
     LIGHT = IMAGE_DICTIONARY["light"]
+    NATION = IMAGE_DICTIONARY["nation"]
     WEATHER = IMAGE_DICTIONARY["weather"]
     GALAXY = IMAGE_DICTIONARY["galaxy"]
     UNIVERSE = IMAGE_DICTIONARY["universe"]
-    IMAGES = [LIGHT, WEATHER, GALAXY, UNIVERSE]
+    IMAGES = [LIGHT, NATION, WEATHER, GALAXY, UNIVERSE]
     TABS = [COORDINATES.tab]
 
     def create(self, image):
         """ Will start creating given element, regardless of next at setting """
         self.go_to_tab()
         coordinates = self.COORDINATES
-        if self.IMAGES.index(image) == self.IMAGES.index(self.WEATHER):
+        helpers.scroll_to_top()
+        if self.IMAGES.index(image) >= self.IMAGES.index(self.NATION):
             pg.moveTo(coordinates.center_of_screen)
-            helpers.scroll_to_top()
             pg.scroll(-2300)  # magic number for weather scroll positions
         elif self.IMAGES.index(image) > self.IMAGES.index(self.WEATHER):
             helpers.scroll_to_bottom()
-        else:
-            helpers.scroll_to_top()
         zone = helpers.get_active_zone(image, 500, 60, coordinates)
         helpers.click_image_in_zone(self.CREATE, zone=zone)
 
@@ -61,6 +60,18 @@ class Creations(Tabs):
         self.go_to_tab()
         zone = helpers.get_active_zone(self.CREATE_CLONES_MAX, 500, 60, self.COORDINATES)
         helpers.click_image_in_zone(self.CREATE_CLONES_ON, zone=zone)
+
+    def check_if_idle(self):
+        """ Defaults to create weather if idle """
+        self.go_to_tab()
+        pos = helpers.return_position_of_image_on_screen(IMAGE_DICTIONARY["creating_shadow_clones"], self.COORDINATES)
+        if pos[0] != -1:
+            # We're creating shadow clones
+            return True
+        else:
+            # We're still in the cycle
+            return False
+
     # def upgrade(self, image):
     #     self.go_to_tab()
     #     if self.IMAGES.index(image) > 1:
